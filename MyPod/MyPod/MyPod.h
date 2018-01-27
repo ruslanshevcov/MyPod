67e03f85541ecdd51276662f0ddf03c5e09008b7
ruslanshevtsov <the_storyteller_@mail.ru>
2018-01-27 07:14:42 +0200

    * Setup project

---
+++ MyPod/MyPod/MyPod.h
@@ -0,0 +1,19 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Ruslan on 1/27/18.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
---
+++ MyPod/MyPod/MyPodClass.swift
@@ -0,0 +1,17 @@
+//
+//  MyPodClass.swift
+//  MyPod
+//
+//  Created by Ruslan on 1/27/18.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class MyPodClass: NSObject {
+
+    func myPodFunc() -> String {
+        return "It's my POD!"
+    }
+    
+}
